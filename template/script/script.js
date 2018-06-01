5640774fd4b28289a1c451f01f1e4fafdcff431d
CodeLibPlan <clplanet26@gmail.com>
2018-06-01 22:06:04 +0900

    스켈레톤 만드는중
    진도가 안나간다
    
    [https://github.com/CodeLibPlan/Board_Picker/issues/4]

--- template/script/script.js
+++ template/script/script.js
@@ -1,27 +1,39 @@
+window.onresize = resize_elements;
+
 function goto_show(event){
-    var now_Y_offset = window.pageYOffset;
-    var goto_offsets = document.getElementById('show').getBoundingClientRect();
-    goto(now_Y_offset,goto_offsets.top);
+    var target = document.getElementById('show');
+    goto(target);
 }
 function goto_add(event){
-    var now_Y_offset = window.pageYOffset;
-    var goto_offsets = document.getElementById('add').getBoundingClientRect();
-    goto(now_Y_offset,goto_offsets.top);
+    var target = document.getElementById('add');
+    goto(target);
 }
 function goto_header(event){
-    var now_Y_offset = window.pageYOffset;
-    var goto_offsets = document.getElementsByTagName('header')[0].getBoundingClientRect();
-    goto(now_Y_offset,goto_offsets.top);
+    var target = document.getElementsByTagName('header')[0];
+    goto(target);
 }
-function goto(starty, endy){
-    var speed = 60;
+function goto(target){
+    var speed = 30;//speed는 반복 횟수. 높으면 느려짐
     var a;
-    a = function (innerspeed, istarty, iendy) {
-        if (innerspeed == 0) return;
-        var diff=iendy-istarty;
-        window.scroll(0, istarty);
-        window.scrollTo(0, istarty +(diff/innerspeed));
-        setTimeout(function(){a(innerspeed - 1, istarty +(diff/innerspeed), iendy)}, 4);
+    var distance = target.getBoundingClientRect().y/speed;
+    a = function (innerspeed, itarget, idistance) {
+        if (innerspeed == 0){
+            if(itarget.getBoundingClientRect().y != 0){
+                console.log(Math.floor(itarget.getBoundingClientRect().top));
+                window.scrollTo(0, itarget.getBoundingClientRect().top+window.pageYOffset);
+            }
+            return;
+        }
+        window.scrollTo(0, window.pageYOffset+(idistance));
+        setTimeout(function(){a(innerspeed - 1, itarget, idistance)}, 10);
     };
-    a(speed, starty, endy);
+    a(speed, target, distance);
+}
+function resize_elements(){
+    var windowheight=window.innerHeight;
+    console.log("asdf");
+    document.getElementsByTagName("header")[0].style.height=windowheight+"px";
+    document.getElementById("show").style.height=windowheight+"px";
+    document.getElementById("add").style.height=windowheight+"px";
+    document.getElementsByTagName("footer")[0].style.height=(windowheight/2)+"px";
 }
