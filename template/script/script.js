5e01514e6bcdc63f03ce11a14984c7233b1ee11e
CodeLibPlan <clplanet26@gmail.com>
2018-06-07 22:37:38 +0900

    템플릿 주석 추가
    템플릿 파일들에 주석을 추가하였습니다.
    주석은 채점 기준에도 들어가기 때문에 코딩하면서 안달아두면 나중에 고생합니다.
    
    [Fixed][https://github.com/CodeLibPlan/Board_Picker/issues/15]

--- template/script/script.js
+++ template/script/script.js
@@ -1,22 +1,26 @@
-window.onresize = resize_elements;
+window.onresize = resize_elements;// 창 크기가 바뀌면 엘리먼트 크기를 리사이즈시킨다.
 
+// 목록 화면으로 이동(스크롤 방식)
 function goto_show(event){
     var target = document.getElementById('show');
     goto(target);
 }
+// 추가 화면으로 이동(스크롤 방식)
 function goto_add(event){
     var target = document.getElementById('add');
     goto(target);
 }
+//헤더로 이동(스크롤 방식)
 function goto_header(event){
     var target = document.getElementsByTagName('header')[0];
     goto(target);
 }
+//주어진 target의 위치로 스크롤하는 메소드
 function goto(target){
     var speed = 30;//speed는 반복 횟수. 높으면 느려짐
     var a;
     var distance = target.getBoundingClientRect().y/speed;
-    a = function (innerspeed, itarget, idistance) {
+    a = function (innerspeed, itarget, idistance) {//재귀 방식으로 구현하기 위해 함수를 새로 정의해 줌.
         if (innerspeed == 0){
             if(itarget.getBoundingClientRect().y != 0){
                 console.log(Math.floor(itarget.getBoundingClientRect().top));
@@ -25,10 +29,11 @@ function goto(target){
             return;
         }
         window.scrollTo(0, window.pageYOffset+(idistance));
-        setTimeout(function(){a(innerspeed - 1, itarget, idistance)}, 10);
+        setTimeout(function(){a(innerspeed - 1, itarget, idistance)}, 10);//10밀리초마다 움직임
     };
     a(speed, target, distance);
 }
+// 화면의 엘리먼트들을 창 크기에 맞춰 주는 메소드
 function resize_elements() {
     var windowheight = window.innerHeight;
     console.log("asdf");
